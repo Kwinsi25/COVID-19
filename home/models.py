3ec128f513d3314e07dc07a6626357e39e6ce0e6
Kwinsi25 <sadhukwinsi25@gmail.com>
2022-03-01 18:03:47 -0500

    Dashboard created

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class staff(models.Model):
+    staffName = models.CharField(primary_key=True,max_length=24)
+    staffContactNumber = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.staffName
+        # fnam mnma lnman status image gender username password
