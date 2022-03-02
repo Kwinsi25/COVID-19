b37143e3a779727913a122781431056304359310
patelomi <91142232+patelomi@users.noreply.github.com>
2022-03-02 21:05:48 +0530

    change in home and added bedAvailability

--- home/views.py
+++ home/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    beds = Bed.objects.all()
+    bedcnt = 0
+    for bed in beds:
+        if bed.occupied == False:
+            bedcnt = bedcnt + 1
+    return render(request, 'index.html',{"bedcnt":bedcnt})
+
 def login(request):
     return render(request,'login.html')
+
+def bedAvailablity(request):
+    beds = Bed.objects.all() 
+    return render(request, 'bedAvailablity.html',{"beds":beds})
