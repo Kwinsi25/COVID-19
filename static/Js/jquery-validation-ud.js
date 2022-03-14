c834ec081554397a6f0b7bb9349d0b008e2a6ba2
ashit452 <ajpatel2468@gmail.com>
2022-03-14 17:30:23 +0530

    gender validation done using jquery

--- static/Js/jquery-validation-ud.js
+++ static/Js/jquery-validation-ud.js
@@ -3,7 +3,7 @@ $().ready(function(){
  
         $("#bookAppointmentForm").validate({
             
-          
+          ignore: [],
           rules : {
             patientName : "required",
             relativeName : "required",
@@ -46,23 +46,23 @@ $().ready(function(){
             
             
             emailId:{
-                required:"please enter email",
-                emailcheck: "please enter valid email"
+                required:"Please enter email",
+                emailcheck: "Please enter valid email"
                 
             },
             gender:{
-                required: "please select gender",
+                required: "Please select gender",
                 
             },
             patientPhone: {
-               required : "please enter mobile no.",
-               minlength: "length of mobile no. should be 10",
-               maxlength: "length of mobile no. should be 10"
+               required : "Please enter mobile no.",
+               minlength: "Length of mobile no. should be 10",
+               maxlength: "Length of mobile no. should be 10"
             },
             relativePhone: {
-                required : "please enter mobile no.",
-                minlength: "length of mobile no. should be 10",
-                maxlength: "length of mobile no. should be 10"
+                required : "Please enter mobile no.",
+                minlength: "Length of mobile no. should be 10",
+                maxlength: "Length of mobile no. should be 10"
              },
              reason : "Please enter Reason to Book Appointment",
             
