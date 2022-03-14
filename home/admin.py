3ec4ee068df62f1308c61b1c34d913a13c155369
patelomi <91142232+patelomi@users.noreply.github.com>
2022-03-14 14:22:22 +0530

    change in admin

--- home/admin.py
+++ home/admin.py
@@ -16,6 +16,7 @@ class configurationAdmin(admin.ModelAdmin):
 
 class CityAdmin(admin.ModelAdmin):
     list_per_page = perpage
+    list_display = ['stateName','cityName']
 
 class StateAdmin(admin.ModelAdmin):
     list_per_page = perpage
@@ -25,39 +26,49 @@ class PatientDocumentAdmin(admin.ModelAdmin):
 
 class PatientSymptomAdmin(admin.ModelAdmin):
     list_per_page = perpage
+    list_display = ['patientName','Symptoms']
 
 class SymptomsAdmin(admin.ModelAdmin):
     list_per_page = perpage
+    list_display = ['symptoms','active']
 
 class staffAdmin(admin.ModelAdmin):
     list_per_page = perpage
+    list_display = ['staffUserName','staffLastName','staffContactNumber','status']
 
 class SpecializationAdmin(admin.ModelAdmin):
     list_per_page = perpage
 
 class DoctorAdmin(admin.ModelAdmin):
     list_per_page = perpage
+    list_display = ['doctorName','doctorUsername','doctorContact','Specialization']
 
 class EquipmentAdmin(admin.ModelAdmin):
     list_per_page = perpage
+    list_display = ['equipment_Name','equipment_Quantity','equipment_Assigned','equipment_Usable']
 
 class OxygenAdmin(admin.ModelAdmin):
     list_per_page = perpage
+    list_display = ['oxygen_Total_Hour','oxygen_Used','oxygen_Remaining','oxygen_Remaining_Hour']
 
 class AppointmentAdmin(admin.ModelAdmin):
     list_per_page = perpage
+    list_display = ['caseNumber','patientName','patientEmail','phone','patientRelativeNumber','reason','dateTime']
 
 class DocumentInline(admin.StackedInline):
     model = PatientDocument
     extra = extrafield
+    list_display = ['patientName','document']
 
 class SymptomInline(admin.StackedInline):
     model = PatientSymptom
     extra = extrafield
     max_num = maxfield
+    list_display = ['patientName','Symptoms']
 
 class BedAdmin(admin.ModelAdmin):
     list_per_page = perpage
+    list_display = ['wardName','bedNumber','occupied']
 
 class PatientAdmin(admin.ModelAdmin):
     list_per_page = perpage
@@ -66,11 +77,13 @@ class PatientAdmin(admin.ModelAdmin):
         
     ]
     inlines = [DocumentInline,SymptomInline]
+    list_display = ['caseNumber','patientName','phone','patientRelativeNumber','bedNumber','doctorName','doctorLastVisited']
 
 class DoctorInline(admin.StackedInline):
     model = WardDoctor
     extra = extrafield
     max_num = maxfield
+    list_display = ['doctorName','doctorUsername','doctorContact','Specialization']
 
 class WardAdmin(admin.ModelAdmin):
     fieldsets = [
